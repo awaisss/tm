2f841efbfebabdcd3f7b54584a1e9758f884784f
ovais <ovais@mobinspire.com>
2017-03-15 13:22:13 +0500

    updates

--- config/cluster.js
+++ config/cluster.js
@@ -46,8 +46,7 @@ module.exports = function(app, config,cluster,sslMode,redis){
 
 	  }
 	  var io = require('socket.io')(server);
-	  /*io.adapter(redis({ host: config.host, port: config.redis }));*/
-	  io.adapter(redis({ host: '35.166.136.116', port: config.redis }));
+	  io.adapter(redis({ host: config.host, port: config.redis }));
 	  var socketIo    	= require('./sockets')(io,cluster);
 	  module.exports 	= require('./express')(app, config);
 
